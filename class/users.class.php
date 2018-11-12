3b40dc427f93029902f8e9eb7ac9bf49288aa435
Renzo VAN WYK <revan-wy@c2r6s3.wethinkcode.co.za>
2018-11-12 09:24:06 +0200

    udpate

--- class/users.class.php
+++ class/users.class.php
@@ -3,6 +3,16 @@
 	//remove before flight
 	ini_set('display_errors', 'On');
 
+	class Users {
 
+		private $db;
+		private 
+		private 
+		private 
+		private 
+		private 
+		public 
+
+	}
 
 ?>
--- pages/home.php
+++ pages/home.php
@@ -1,7 +1,7 @@
 <?php //untested and incomplete
 
 	//remove before flight
-	ini_set('display_errors', 'On');
+	ini_set('display_errors', 'On')
 
 	session_start();
 	require_once '../class/users.class.php'; //file incomplete
@@ -13,13 +13,38 @@
 		<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 		<link rel="stylesheet" href="../public/css/home.css"> <!--file incomplete-->
 		<link rel="stylesheet" href="../public/css/headerfooter.css"> <!--file incomplete-->
-		<title>Camagru Login</title>
+		<title>Camagru Login2</title>
 	</head>
 	<body>
 		<h1 class=title id=maintitle>Camagru</h1>
 		<div class="login">
 			<h2>Log In</h2>
-			<!--START HERE-->
+			<form class="" action="#" method="post">
+				User Name<br /><input type="text" name="login" value=""><br />
+				Password<br /><input type="password" name="passwd" value=""><br /><br />
+				<input class="button" type="submit" name="submit" value="OK">
+			</form>
+			<a href="forgot.php" id="forgot">Forgot Password?</a> <!--file incomplete-->
 		</div>
+		<?php
+			if (isset($_POST['login']) and isset($_POST['passwd']))
+			{
+				if (!empty(htmlentities($_POST['login'])) and !empty(htmlentities	($_POST['passwd'])) and $_POST['submit'] == "OK")
+				{
+					$login = trim(htmlentities($_POST['login']));
+					$passwd = htmlentities($_POST['passwd']);
+					$db = new Users($login, $passwd, "", "", "");
+					$db->connectUser();
+					if ($db->message)
+						echo '<div style="color:red;">'.$db->message.'</div>';
+					else
+					{
+						$_SESSION['logged_user'] = $login;
+						echo '<script> location.replace(../index.php); </script>';
+					}
+				}
+			}
+		?>
+
 	</body>
 </html>
