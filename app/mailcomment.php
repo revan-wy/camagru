6e12fcb8c68e74d6aaac2bf33647578d76967321
Renzo VAN WYK <revan-wy@c1r4s4.wethinkcode.co.za>
2018-11-16 11:51:21 +0200

    update

--- app/mailcomment.php
+++ app/mailcomment.php
@@ -5,7 +5,7 @@
 
 	$headers = 'MIME-Version: 1.0'."\n";
 	$headers .= 'Content-type: text/html; charset=iso-8859-1'."\n";
-	$headers .= "From: camagru-noreply@student.42.fr\n";
+	$headers .= "From: camagru-noreply@student.wethinkcode.co.za\n";
 	$mailbody = "<html><body>";
 	$mailbody .= "<p>Hi, ".$login_pic."</p>";
 	$mailbody .= "<p>".$this->login." posted a new comment on your picture: \"".$this->comment."\"</p>";
