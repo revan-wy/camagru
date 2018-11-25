de329993ff6cb89dcad833e356768ebeea14568f
Renzo VAN WYK <revan-wy@c2r6s3.wethinkcode.co.za>
2018-11-25 09:49:59 +0200

    update

--- codecourse/js/photo.js
+++ codecourse/js/photo.js
@@ -27,4 +27,18 @@
 			video.play();
 		});
 	}
+
+	var canvas = document.getElementById('canvas');
+	var context = canvas.getContext('2d');
+	var video = document.getElementById('video');
+	document.getElementById("snap").addEventListener("click", function() {
+		context.drawImage(video, 0, 0, 640, 480);
+		var image = new Image();
+		image.src = canvas.toDataURL("image/png");
+		/*var canvas2 = document.createElement("canvas2");
+		canvas2.width = image.width;
+		canvas2.height = image.height;
+		canvas2.getContext("2d").drawImage(image, 0, 0);
+		return canvas2;*/
+	});
 })();
