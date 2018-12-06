5728aea3bffc30dbc4836882ff19bb49460ba801
Renzo VAN WYK <revan-wy@c2r6s3.wethinkcode.co.za>
2018-12-06 19:03:53 +0200

    working on comments in mygallery

--- pages/mygallery.php
+++ pages/mygallery.php
@@ -14,7 +14,7 @@
 		<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 		<link rel="stylesheet" href="../public/css/gallery.css">
 		<link rel="stylesheet" href="../public/css/headerfooter.css">
-		<title>Camagru Main Gallery</title>
+		<title>Camagru - My Gallery</title>
 	</head>
 	<body>
 		<?php include 'header.php'; ?>
