6fce1ab96958fb72e740b2a26c17c8bd353206a9
Renzo VAN WYK <revan-wy@c2r6s3.wethinkcode.co.za>
2019-01-03 16:12:25 +0200

    updated charset in mail headers

--- app/mailcomment.php
+++ app/mailcomment.php
@@ -4,7 +4,7 @@
 	ini_set('display_errors', 'On');
 
 	$headers = 'MIME-Version: 1.0'."\n";
-	$headers .= 'Content-type: text/html; charset=iso-8859-1'."\n";
+	$headers .= 'Content-type: text/html; charset=UTF-8'."\n";
 	$headers .= "From: camagru-noreply@student.wethinkcode.co.za\n";
 	$mailbody = "<html><body>";
 	$mailbody .= "<p>Hi, ".$login_pic."</p>";
